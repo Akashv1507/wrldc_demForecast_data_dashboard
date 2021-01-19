4fd3e724925ca970d5cc3149f7597cbcddf1731a
Akash Verma <akashv1507@gmai.com>
2021-01-20 00:34:23 +0530

    minor changes

--- setup_service.bat
+++ setup_service.bat
@@ -1,5 +1,5 @@
-call nssm.exe install mis_data_dashboard "%cd%\run_server.bat"
-rem call nssm.exe edit mis_data_dashboard
-call nssm.exe set mis_data_dashboard AppStdout "%cd%\logs\mis_data_dashboard.log"
-call nssm.exe set mis_data_dashboard AppStderr "%cd%\logs\mis_data_dashboard.log"
-call sc start mis_data_dashboard
+call nssm.exe install mis_errorData_dashboard "%cd%\run_server.bat"
+rem call nssm.exe edit mis_errorData_dashboard
+call nssm.exe set mis_errorData_dashboard AppStdout "%cd%\logs\mis_errorData_dashboard.log"
+call nssm.exe set mis_errorData_dashboard AppStderr "%cd%\logs\mis_errorData_dashboard.log"
+call sc start mis_errorData_dashboard
